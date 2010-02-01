b6cf85f68f558c3a844ec45a5611cb26f0ec0856
limitusus <limit.usus@gmail.com>
2010-02-01 13:12:20 +0900

    command-line args

--- udpcli.py
+++ udpcli.py
@@ -1,5 +1,9 @@
+import sys
 from socket import *
 
+if len(sys.argv) < 4 :
+    print "Usage : %s HOST PORT MESSAGE" % (sys.argv[0])
+    sys.exit(1)
+
 us = socket(AF_INET, SOCK_DGRAM, 0)
-for i in range(0,100) :
-    us.sendto("hoge", ("localhost", 3000))
+us.sendto(sys.argv[3], (sys.argv[1], int(sys.argv[2])))
--- udpsrv.py
+++ udpsrv.py
@@ -1,11 +1,16 @@
+import sys
 from socket import *
 
+if len(sys.argv) < 3 :
+    print "Usage : %s PORT" % (sys.argv[0])
+    sys.exit(1)
+
 us = socket(AF_INET, SOCK_DGRAM, 0)
-us.bind(("localhost", 3000))
+us.bind(("localhost", int(sys.argv[1])))
 
 recv_counter = 0
+print us
 while True:
     s = us.recvfrom(1024)
     recv_counter += 1
-    print recv_counter, s
-
+    print recv_counter, s, s
